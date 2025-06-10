b4684758f490fc56de53781e69b654f28d0e9d7c
Chris794 <christhopper2004@gmail.com>
2025-06-09 21:51:57 -0300

    technicians(models): parte 1 – criar modelo Tecnico

--- apps/technicians/models.py
+++ apps/technicians/models.py
@@ -6,6 +6,7 @@ class Tecnico(models.Model):
     nome = models.CharField(max_length=100)
     especializacao = models.CharField(max_length=100)
     telefone = models.CharField(max_length=20)
+# technicians(models): parte 1 – criar modelo Tecnico
 
     def __str__(self):
         return self.nome
