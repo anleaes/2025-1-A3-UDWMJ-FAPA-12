de67c78e4c77dd83c78aa7abb72c4901fca359fa
Chris794 <christhopper2004@gmail.com>
2025-06-09 21:39:53 -0300

    equipments(models): parte 1 – criar modelo Equipamento

--- apps/equipments/models.py
+++ apps/equipments/models.py
@@ -6,6 +6,7 @@ class Equipamento(models.Model):
     numero_serie = models.CharField(max_length=100)
     modelo = models.CharField(max_length=100)
     localizacao = models.CharField(max_length=200)
+# equipments(models): parte 1 – criar modelo Equipamento
 
     def __str__(self):
         return f"{self.modelo} - {self.numero_serie}"
